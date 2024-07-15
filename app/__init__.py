623e65fc1870683e4bc8efe7daf00d96458bc9e3
Mr-Bazoo <vanbeersbas@gmail.com>
2024-07-15 21:12:24 +0200

    meh

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,25 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
-)
-
-from app import routes, models
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    
+    limiter = Limiter(
+        get_remote_address,
+        app=app,
+        default_limits=["200 per day", "50 per hour"]
+    )
+    
+    with app.app_context():
+        from app import routes, models
+        db.create_all()
+    
+    return app
--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'een_zeer_geheime_sleutel'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///portfolio.db'
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
